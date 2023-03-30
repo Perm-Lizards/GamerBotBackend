7cc1e5b7d5abd8bda29c0896a8a7baa8dd2d41ea
Koichi-hub <gogotimur2020@gmail.com>
2023-03-31 03:12:52 +0500

    Удалена папка handlers чтобы логика была в роутах

--- src/handlers/profilesHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export const get = (req: Request, res: Response) => {
-  //
-};
-
-export const getById = (req: Request, res: Response) => {
-  res.send(req.params.id);
-};
