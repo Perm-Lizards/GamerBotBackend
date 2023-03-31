9f7af36400eef794b765c29dffdfbb889541233c
Koichi-hub <gogotimur2020@gmail.com>
2023-03-31 20:14:37 +0500

    Написал profiles API

--- src/routes/profiles.ts
+++ src/routes/profiles.ts
@@ -8,17 +8,6 @@ const prisma = new PrismaClient();
 
 router
   .route('/profiles')
-  // Get метод надо удалить, это сниппет, потом перенесем в projects
-  .get(async (_, res) => {
-    const profiles = await prisma.profiles.findMany();
-
-    res.json(
-      profiles.map(p => ({
-        ...p,
-        logo: p.logo.toString('base64'),
-      }))
-    );
-  })
   .post(async (req, res) => {
     const profile = req.body;
 
@@ -32,8 +21,84 @@ router
     });
 
     res.status(StatusCode.SuccessOK).end();
+  })
+  .put(async (req, res) => {
+    const profile = req.body;
+
+    await prisma.profiles.update({
+      where: {
+        id: profile.id,
+      },
+      data: profile,
+    });
+
+    res.status(StatusCode.SuccessOK).end();
   });
 
-router.route('/profiles/:id').get().put().delete();
+router
+  .route('/profiles/:id')
+  .get(async (req, res) => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+      res.status(StatusCode.ClientErrorBadRequest).end();
+      return;
+    }
+
+    try {
+      const profile = await prisma.profiles.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      });
+
+      res.status(StatusCode.SuccessOK).json({
+        ...profile,
+        logo: profile.logo.toString('base64'),
+      });
+    } catch (e) {
+      res.status(StatusCode.SuccessNoContent).end();
+    }
+  })
+  .delete(async (req, res) => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+      res.status(StatusCode.ClientErrorBadRequest).end();
+      return;
+    }
+
+    await prisma.profiles.delete({
+      where: {
+        id,
+      },
+    });
+
+    res.status(StatusCode.SuccessOK).end();
+  });
+
+router.route('/profiles/:id/projects').get(async (req, res) => {
+  const id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    res.status(StatusCode.ClientErrorBadRequest).end();
+    return;
+  }
+
+  try {
+    const projects = await prisma.profiles.findUniqueOrThrow({
+      where: {
+        id,
+      },
+      select: {
+        projects: true,
+      },
+    });
+
+    res.status(StatusCode.SuccessOK).json(projects);
+  } catch (e) {
+    res.status(StatusCode.SuccessNoContent).end();
+  }
+});
 
 export default router;
