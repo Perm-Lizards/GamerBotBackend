7377bd7f53aba74a3f93498fdffab46ab85cbf98
Koichi-hub <gogotimur2020@gmail.com>
2023-04-03 02:41:42 +0500

    Проектирование API по REST
    Добавлен swagger.json
    Добавлен Swagger UI

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import profiles from './profiles';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from '../../swagger.json';
 
 const router = express.Router();
 
 router.use('/api', [profiles]);
+router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 export default router;
